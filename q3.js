f18548014e37886ebf07571e79639d2758845fdf
Harsh <Harsh>
2026-09-22 17:18:09 +0530

    q3.js

---
+++ q3.js
@@ -0,0 +1,23 @@
+menuChoice = (choice) => {
+    switch (choice) {
+
+        case 1:
+            console.log("Start Game");
+            break
+        case 2:
+            console.log("Load Game");
+            break
+
+        case 3:
+            console.log("Settings");
+            break
+        case 4:
+            console.log("Exit")
+            break
+
+        default:
+            console.log("Invalid Choice")
+            break
+    }
+}
+menuChoice(3)
