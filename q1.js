96e874d502ddabf7b18df55c3546d33d5657e89d
Harsh <Harsh>
2026-09-22 16:50:58 +0530

    Basic Javascript Program

---
+++ q1.js
@@ -0,0 +1,14 @@
+const student_name = "Harsh Kr";
+let Age =17;
+let Course ="B-Tech Cse";
+const College_name ="SITM";
+let Marks =78;
+const Birth_Year = "21/09/2008"
+Age =18;
+Marks =90;
+console.log("My Name is : ",student_name)
+console.log("Birth Date :",Birth_Year)
+console.log("Age : ",Age)
+console.log(College_name)
+console.log(Course)
+console.log(Marks)
