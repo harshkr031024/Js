020aaf527fd7d23aef8902df85222d91dceaacff
Harsh <Harsh>
2026-09-24 16:05:54 +0530

    q6

---
+++ q6.js
@@ -0,0 +1,12 @@
+classifyNumber = (number) =>{
+    if (number >0) {
+        console.log("Positive Number ");
+    }
+    else if (number <0) {
+        console.log("Negative Number ");
+    }
+    else {
+        console.log("Zero")
+    }
+}
+classifyNumber(-8)
