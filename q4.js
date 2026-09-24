f76aa0ca2dfee2a90da82c169327791221887a21
Harsh <Harsh>
2026-09-24 15:19:00 +0530

    day04

--- q4.js
+++ q4.js
@@ -1,16 +1,15 @@
 findNumber = (numbers,target) => {
     for (const i of numbers) {
-        if (i == target){
+        if (i < 0) {
+            continue
+        }
+        else if (i == target){
             console.log("Found")
             break
         }
-        else if (i < 0) {
-            continue
-        }
         else {
             console.log("Not  Found")
-            break
         }
     }
 }
-findNumber([1,4,7,9],5)
+findNumber([-5,10,-2,7,20],7)
